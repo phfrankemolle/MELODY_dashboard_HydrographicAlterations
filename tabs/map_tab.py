7e8f3bedbb83307ea4417d63700bdb8c943595c1
phfrankemolle <94836035+phfrankemolle@users.noreply.github.com>
2026-05-21 16:20:57 +0200

    Update map_tab.py

--- tabs/map_tab.py
+++ tabs/map_tab.py
@@ -345,7 +345,7 @@ def show_sandwave_tool():
     # ---------------------------------------------------------
     # TWO COLUMNS
     # ---------------------------------------------------------
-    with st.popover("Instellingen"):
+    with st.sidebar:
     
         col1, col2 = st.columns(2)
         
