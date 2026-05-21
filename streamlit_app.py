b7c1d4ebf04b8300fd09be59f44571f15f221d14
phfrankemolle <94836035+phfrankemolle@users.noreply.github.com>
2026-05-21 17:49:31 +0200

    Update streamlit_app.py
    changed part of the UI to the sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,13 @@ from tabs.time_tab import show_time_series
 from tabs.map_tab import show_sandwave_tool
 
 st.set_page_config(layout="wide")
-st.header("MELODY maps")
-
-tab = st.selectbox(
-    "Kies functie",
-    ["Kaarten", "Tijdreeksen"]
-)
+with st.sidebar:
+    st.header("MELODY maps")
+    
+    tab = st.selectbox(
+        "Kies functie",
+        ["Kaarten", "Tijdreeksen"]
+    )
 
 if tab == "Kaarten":
     show_sandwave_tool()
