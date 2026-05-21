6c523bcc082849767481f00c1eaa2feba44a286d
phfrankemolle <94836035+phfrankemolle@users.noreply.github.com>
2026-05-21 13:12:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+from tabs.time_tab import show_time_series
+from tabs.map_tab import show_sandwave_tool
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(layout="wide")
+st.header("MELODY maps")
+
+tab = st.selectbox(
+    "Kies functie",
+    ["Kaarten", "Tijdreeksen"]
 )
+
+if tab == "Kaarten":
+    show_sandwave_tool()
+    
+elif tab == "Tijdreeksen":
+    show_time_series()
+
