3de69ee512e36cbff4bc9024430d39ffc60a3c79
phfrankemolle <94836035+phfrankemolle@users.noreply.github.com>
2026-07-10 14:38:23 +0200

    Update streamlit_app.py
    fix UI buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,23 +3,19 @@ from tabs.time_tab import show_time_series
 from tabs.map_tab import show_sandwave_tool
 from tabs.about_tab import about
 st.set_page_config(layout="wide")
-
-col1, col2 = st.columns([1, 7])
-with col1:
-    with st.sidebar:
-        st.markdown(
-        "<h2 style='font-size: 36px;'>MELODY Maps</h2>",
-        unsafe_allow_html=True
-        )
+with st.sidebar:
+    st.markdown(
+    "<h2 style='font-size: 36px;'>MELODY Maps</h2>",
+    unsafe_allow_html=True
+    )
     
     tab = st.selectbox(
         "Kies functie",
         ["Kaarten", "Tijdreeksen"]
     )
 
-with col2:
-    if st.button("ℹ️", help="About MELODY"):
-        about()
+if st.button("ℹ️", help="About MELODY"):
+    about()
 
 if tab == "Kaarten":
     show_sandwave_tool()
