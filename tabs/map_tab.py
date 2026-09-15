5155a6d2021540961c1ff40498d2b80c842fb5b7
phfrankemolle <94836035+phfrankemolle@users.noreply.github.com>
2026-09-15 15:46:33 +0200

    Update map_tab.py

--- tabs/map_tab.py
+++ tabs/map_tab.py
@@ -551,7 +551,7 @@ def show_sandwave_tool():
             "variable": st.session_state.get("fig1_var"),
             "scenario": st.session_state.get("fig1_scenario")
         }
-        st.write(st.session_state.get("fig1_scenario")+" "+st.session_state.get("fig1_year") +"\nLine"
+        st.write(st.session_state.get("fig1_scenario")+" "+st.session_state.get("fig1_year") +", "
         +st.session_state.get("fig1_var")+" "+st.session_state.get("fig1_button"))
         if settings_left["button"] and settings_left["year"]:
             base_path = resolve_base_path(settings_left)
@@ -600,7 +600,7 @@ def show_sandwave_tool():
             "variable": st.session_state.get("fig2_var"),
             "scenario": st.session_state.get("fig2_scenario")
         }
-        st.write(st.session_state.get("fig2_scenario")+" "+st.session_state.get("fig2_year") +"\nLine"
+        st.write(st.session_state.get("fig2_scenario")+" "+st.session_state.get("fig2_year") +", "
         +st.session_state.get("fig2_var")+" "+st.session_state.get("fig2_button"))
         if settings_right["button"] and settings_right["year"]:
             base_path = resolve_base_path(settings_right)
