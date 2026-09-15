1ef38a8b17d3c84ccac5c0a3bd7c30d4ec4399dc
phfrankemolle <94836035+phfrankemolle@users.noreply.github.com>
2026-09-16 00:35:38 +0200

    Update map_tab.py

--- tabs/map_tab.py
+++ tabs/map_tab.py
@@ -567,13 +567,13 @@ def show_sandwave_tool():
     col1_tit1, col2_tit1 = st.columns(2) #title column 1
     with col1_tit1:
         if st.session_state.get("fig1_button") == "Bathymetrie":
-            continue
+            pass
         else:
             st.write(st.session_state.get("fig1_scenario")+" "+st.session_state.get("fig1_year") +";")
 
     with col2_tit1:
         if st.session_state.get("fig2_button") == "Bathymetrie":
-            continue
+            pass
         else:
             st.write(st.session_state.get("fig2_scenario")+" "+st.session_state.get("fig2_year") +";")
     
