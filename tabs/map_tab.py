8e76dbc3cf37470f66b4cb6f30dfed61f231dc2b
phfrankemolle <94836035+phfrankemolle@users.noreply.github.com>
2026-09-15 15:36:55 +0200

    Update map_tab.py
    attempt to add titles above figures

--- tabs/map_tab.py
+++ tabs/map_tab.py
@@ -551,12 +551,9 @@ def show_sandwave_tool():
             "variable": st.session_state.get("fig1_var"),
             "scenario": st.session_state.get("fig1_scenario")
         }
-    
+        st.write(st.session_state.get("fig1_scenario")+" "+st.session_state.get("fig1_year"))
+        st.write(st.session_state.get("fig1_var")+" "+st.session_state.get("fig1_button"))
         if settings_left["button"] and settings_left["year"]:
-            st.write(
-                "fig1 scenario:",
-                st.session_state.get("fig1_scenario")
-            )
             base_path = resolve_base_path(settings_left)
             wind_path = resolve_wind_overlay(settings_left)
             threshold_path = resolve_threshold_overlay(settings_left)
@@ -603,7 +600,8 @@ def show_sandwave_tool():
             "variable": st.session_state.get("fig2_var"),
             "scenario": st.session_state.get("fig2_scenario")
         }
-    
+        st.write(st.session_state.get("fig2_scenario")+" "+st.session_state.get("fig2_year"))
+        st.write(st.session_state.get("fig2_var")+" "+st.session_state.get("fig2_button"))
         if settings_right["button"] and settings_right["year"]:
             base_path = resolve_base_path(settings_right)
             wind_path = resolve_wind_overlay(settings_right)
